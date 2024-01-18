f509729a62a93ba6744d5982d6ebd8ca8b8c1767
Jaromír Janisch <jaara.j@gmail.com>
2024-01-18 15:35:24 +0100

    updated the HUGE_GEN_RGOAL_STOCH scenario

--- src/nasimemu/nasim/scenarios/benchmark/generated.py
+++ src/nasimemu/nasim/scenarios/benchmark/generated.py
@@ -40,7 +40,7 @@ TINY_GEN = {**DEFAULTS,
             "num_processes": 1,
             "restrictiveness": 1}
 TINY_GEN_RGOAL = {**DEFAULTS,
-                  "name": "tiny-gen-rangoal",
+                  "name": "tiny-gen-rgoal",
                   "num_hosts": 3,
                   "num_os": 1,
                   "num_services": 1,
@@ -55,7 +55,7 @@ SMALL_GEN = {**DEFAULTS,
              "num_processes": 2,
              "restrictiveness": 2}
 SMALL_GEN_RGOAL = {**DEFAULTS,
-                   "name": "small-gen-rangoal",
+                   "name": "small-gen-rgoal",
                    "num_hosts": 8,
                    "num_os": 2,
                    "num_services": 3,
@@ -86,6 +86,7 @@ HUGE_GEN = {**DEFAULTS,
             "num_processes": 4,
             "restrictiveness": 3,
             "step_limit": 10000}
+       
 POCP_1_GEN = {**DEFAULTS,
               "name": "pocp-1-gen",
               "num_hosts": 35,
@@ -105,25 +106,43 @@ POCP_2_GEN = {**DEFAULTS,
               "restrictiveness": 5,
               "step_limit": 30000}
 
-MEDIUM_GEN = {**DEFAULTS,
+# added
+MEDIUM_GEN_RGOAL = {**DEFAULTS,
               "name": "medium-gen-rgoal",
               "num_hosts": 16,
               "num_os": 2,
               "num_services": 5,
               "num_processes": 2,
               "restrictiveness": 3,
-              "step_limit": 2000,
+              "step_limit": 2000, # not used
               "random_goal": True,
               "address_space_bounds": (12, 6)}
+
+# this is the scenario used in dissertation Janisch 2024
+HUGE_GEN_RGOAL_STOCH = {**DEFAULTS,
+            "name": "huge-gen-rgoal",
+            "num_hosts": 38,
+            "num_os": 4,
+            "num_services": 10,
+            "num_processes": 4,
+            "restrictiveness": 3,
+            "base_host_value": 0.,
+            "host_discovery_value": 0.,
+            "privesc_probs": 0.8,
+            "exploit_probs": 0.8,
+            "random_goal": True,
+            "step_limit": 10000, # not used -- disabled in env.py `scenario_params['step_limit'] = None`
+            "address_space_bounds": (12, 6)} # subnet sizes are randomly in [4-6]
 
 AVAIL_GEN_BENCHMARKS = {
     "tiny-gen": TINY_GEN,
     "tiny-gen-rgoal": TINY_GEN_RGOAL,
     "small-gen": SMALL_GEN,
     "small-gen-rgoal": SMALL_GEN_RGOAL,
-    "medium-gen": MEDIUM_GEN,
+    "medium-gen-rgoal": MEDIUM_GEN_RGOAL,
     "large-gen": LARGE_GEN,
     "huge-gen": HUGE_GEN,
+    "huge-gen-rgoal-stoch": HUGE_GEN_RGOAL_STOCH,
     "pocp-1-gen": POCP_1_GEN,
     "pocp-2-gen": POCP_2_GEN
 }
